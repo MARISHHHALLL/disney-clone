21f79e702002354a2fccafb76ec77f39d35fed51
Marishal2 <kouraybane809@gmail.com>
2021-10-27 00:11:24 +0100

    adding:video content in logos

--- src/mainSection/Logos.js
+++ src/mainSection/Logos.js
@@ -6,10 +6,13 @@ const Logos = () => {
   return (
     <LgosCon>
       {data.map((dt) => {
-        const { id, slogo } = dt
+        const { id, slogo, svideo } = dt
         return (
           <Cont key={id}>
             <img src={slogo} alt='' />
+            <video autoPlay='true' muted loop='true' playsInline='true'>
+              <source src={svideo} type='video/mp4' />
+            </video>
           </Cont>
         )
       })}
@@ -28,16 +31,41 @@ const LgosCon = styled.div`
 const Cont = styled.div`
   cursor: pointer;
   border: 4px solid rgba(249, 249, 249, 0.1);
-
+  padding-top: 56.25%;
   border-radius: 10px;
   transition: all 250ms cubic-bezier(0.67, 0.22, 0.3, 0.97) 0s;
-  &:hover {
-    transform: scale(1.05);
-    border: 4px solid rgba(249, 249, 249, 0.8);
-  }
+  position: relative;
+  overflow: hidden;
+
   img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
+    z-index: 1;
+    top: 0;
+  }
+  video {
     height: 100%;
     width: 100%;
     object-fit: cover;
+    position: absolute;
+    top: 0;
+    z-index: 0;
+    opacity: 0;
+    visibility: hidden;
+  }
+  &:hover {
+    transform: scale(1.05);
+    border: 4px solid rgba(249, 249, 249, 0.8);
+
+    video {
+      opacity: 1;
+      visibility: visible;
+    }
   }
 `
--- src/mainSection/LogosData.js
+++ src/mainSection/LogosData.js
@@ -2,22 +2,27 @@ const LogosData = [
   {
     id: 1,
     slogo: '/images/viewers-disney.png',
+    svideo: '/videos/disney.mp4',
   },
   {
     id: 2,
     slogo: '/images/viewers-marvel.png',
+    svideo: '/videos/marvel.mp4',
   },
   {
     id: 3,
     slogo: '/images/viewers-pixar.png',
+    svideo: '/videos/pixar.mp4',
   },
   {
     id: 4,
     slogo: '/images/viewers-starwars.png',
+    svideo: '/videos/star-wars.mp4',
   },
   {
     id: 5,
     slogo: '/images/viewers-national.png',
+    svideo: '/videos/national-geographic.mp4',
   },
 ]
 export default LogosData
